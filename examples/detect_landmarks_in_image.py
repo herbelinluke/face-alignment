51dde60dbe825fccc71239a5611bf83ea2ff2c12
Luke Herbelin <herbelinluke@gmail.com>
2025-04-25 19:30:02 -0700

    expression girlB

--- examples/detect_landmarks_in_image.py
+++ examples/detect_landmarks_in_image.py
@@ -13,19 +13,20 @@ face_detector_kwargs = {
 }
 
 # Run the 3D face alignment on a test image, without CUDA.
-fa = face_alignment.FaceAlignment(face_alignment.LandmarksType.THREE_D, device='cpu', flip_input=True,
+fa = face_alignment.FaceAlignment(face_alignment.LandmarksType.TWO_D, device='cpu', flip_input=True,
                                   face_detector=face_detector, face_detector_kwargs=face_detector_kwargs)
 
 try:
-    input_img = io.imread('~/Documents/face-alignment/test/assets/girlBsurg.png')
+#    input_img = io.imread('~/Documents/face-alignment/test/assets/girlBsurg.png')
+    input_img = io.imread('~/Downloads/girlBsurg_initialize.png')
 except FileNotFoundError:
     input_img = io.imread('test/assets/00016.png')
 
 start_time = time.time()
 preds = fa.get_landmarks(input_img)[-1]
-#np.save('girlBsurg.npy', preds)
+np.save('girl2Bsurg.npy', preds)
 nojaw_preds = preds[17:]
-#np.save('girlBnoJaw.npy', nojaw_preds)
+np.save('girl2BnoJaw.npy', nojaw_preds)
 time.sleep(2)
 end_time = time.time()
 elapsed_time = end_time - start_time
@@ -49,31 +50,31 @@ pred_types = {'face': pred_type(slice(0, 17), (0.682, 0.780, 0.909, 0.5)),
               'teeth': pred_type(slice(60, 68), (0.596, 0.875, 0.541, 0.4))
               }
 
-fig = plt.figure(figsize=plt.figaspect(.5))
-ax = fig.add_subplot(1, 2, 1)
-ax.imshow(input_img)
-
-for pred_type in pred_types.values():
-    ax.plot(preds[pred_type.slice, 0],
-            preds[pred_type.slice, 1],
-            color=pred_type.color, **plot_style)
-
-ax.axis('off')
-
-# 3D-Plot
-ax = fig.add_subplot(1, 2, 2, projection='3d')
-surf = ax.scatter(preds[:, 0] * 1.2,
-                  preds[:, 1],
-                  preds[:, 2],
-                  c='cyan',
-                  alpha=1.0,
-                  edgecolor='b')
-
-for pred_type in pred_types.values():
-    ax.plot3D(preds[pred_type.slice, 0] * 1.2,
-              preds[pred_type.slice, 1],
-              preds[pred_type.slice, 2], color='blue')
-
-ax.view_init(elev=90., azim=90.)
-ax.set_xlim(ax.get_xlim()[::-1])
-plt.show()
+#fig = plt.figure(figsize=plt.figaspect(.5))
+#ax = fig.add_subplot(1, 2, 1)
+#ax.imshow(input_img)
+#
+#for pred_type in pred_types.values():
+#    ax.plot(preds[pred_type.slice, 0],
+#            preds[pred_type.slice, 1],
+#            color=pred_type.color, **plot_style)
+#
+#ax.axis('off')
+#
+## 3D-Plot
+#ax = fig.add_subplot(1, 2, 2, projection='3d')
+#surf = ax.scatter(preds[:, 0] * 1.2,
+#                  preds[:, 1],
+#                  preds[:, 2],
+#                  c='cyan',
+#                  alpha=1.0,
+#                  edgecolor='b')
+#
+#for pred_type in pred_types.values():
+#    ax.plot3D(preds[pred_type.slice, 0] * 1.2,
+#              preds[pred_type.slice, 1],
+#              preds[pred_type.slice, 2], color='blue')
+#
+#ax.view_init(elev=90., azim=90.)
+#ax.set_xlim(ax.get_xlim()[::-1])
+#plt.show()
